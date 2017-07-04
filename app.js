3717d0264c0a63a8fd7a539cd13c467f3b308bd3
nramlnhmd <nramlnhmd@gmail.com>
2017-07-04 11:47:40 +0800

    This is the new directory structure

---
+++ app.js
@@ -0,0 +1,32 @@
+var connect = require('connect');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
+var path = require('path');
+var bodyParser = require( 'body-parser' );
+var nodemailer = require( 'nodemailer' );
+var cors = require('cors');
+var http = require("http").createServer(app);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Credentials', false);
+  next();
+});
+
+app.use(bodyParser.json({ limit: '50mb', extended: true, type:'application/json' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, type:'application/x-www-form-urlencoding' }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ limit: '50mb' }));
+
+var www = connect();
+www.use(serveStatic('www'));
+app.use('/', www);
+
+app.listen(5000, function () {
+  console.log('CORS-enabled web server listening on port 5000')
+})
+console.log("Magic at 5000");

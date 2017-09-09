f5543d9916f5d62734a6c495ebe04bf9a6e17994
amolc <amol.chawathe@fountaintechies.com>
2017-09-09 23:29:56 +0800

    Added a live chat to our website

--- www/wp-content/themes/consultingx/assets/js/init.js
+++ www/wp-content/themes/consultingx/assets/js/init.js
@@ -154,22 +154,26 @@
   ga('create', 'UA-80556549-1', 'auto');
   ga('send', 'pageview');
 //drip
-
-<!-- Drip -->
-
-  var _dcq = _dcq || [];
-  var _dcs = _dcs || {};
-  _dcs.account = '8321936';
-
-  (function() {
-    var dc = document.createElement('script');
-    dc.type = 'text/javascript'; dc.async = true;
-    dc.src = '//tag.getdrip.com/8321936.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(dc, s);
-  })();
+//
+// <!-- Drip -->
+//
+//   var _dcq = _dcq || [];
+//   var _dcs = _dcs || {};
+//   _dcs.account = '8321936';
+//
+//   (function() {
+//     var dc = document.createElement('script');
+//     dc.type = 'text/javascript'; dc.async = true;
+//     dc.src = '//tag.getdrip.com/8321936.js';
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(dc, s);
+//   })();
 
 // end document
 
 window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
 heap.load("3375695941");
+
+// livechat
+
+<script src="https://my.marketeer.co/interface/?k=0x179a19a7b"></script>

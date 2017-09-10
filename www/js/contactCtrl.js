263703be9df6f1be9a157090f117c9960dc9b66e
amolc <amol.chawathe@fountaintechies.com>
2017-09-10 12:21:50 +0800

    Adding a new event in js

--- www/js/contactCtrl.js
+++ www/js/contactCtrl.js
@@ -66,6 +66,30 @@ app.controller('contactcontroller', function ($scope, $http, $window) {
     $("#thankyou").delay(3200).hide(300);
   }
 
+  // Bangalore event
+
+  //----------- Consult ------------------------///
+
+  $scope.event1 = function (req, res) {
+    $scope.data = {};
+    $scope.data.firstname = $scope.firstName;
+    $scope.data.lastname = $scope.lastName;
+    $scope.data.email = $scope.email;
+    $scope.data.phonenumber = $scope.phoneNumber;
+    $scope.data.message = $scope.description;
+
+
+    $http.post(baseurl + 'expand3/', $scope.data).success(function (res) {
+      if (res.status == 'false') {
+      }
+    }).error(function () {
+      console.log("error");
+    })
+    document.contactform.reset();
+    $("#thankyou").show();
+    $("#thankyou").delay(3200).hide(300);
+  }
+
 
   //----------- Success message function ------------------------///
   $scope.successMsg = function () {
